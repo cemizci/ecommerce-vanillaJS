b827a9975faaca7e016e58ae00544cc99c2e39b1
Cem <cmizci@gmail.com>
2025-01-19 04:29:58 +0300

    product-detail

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector("#sideBar")
+const mobileMenu = document.querySelector("#mobileMenu")
+
+menuBtn.addEventListener('click', () => {
+    console.log("sjkjsndjkn")
+});
