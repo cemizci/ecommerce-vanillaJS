a96e883894af1270dd17b4cfd3d2a8518dc9dbfc
Cem <cmizci@gmail.com>
2025-01-24 05:35:41 +0300

    start js file

--- js/index.js
+++ js/index.js
@@ -1,6 +1,48 @@
 const menuBtn = document.querySelector("#sideBar")
 const mobileMenu = document.querySelector("#mobileMenu")
+const closeSideBar = document.querySelector("#closeSidebar")
+const modalSearch = document.getElementsByClassName("modal-search")
+const searchBtn = document.querySelector(".search-button")
+const modalCloseBtn = document.querySelector('#modalCloseBtn')
+const modalSearchWrapper = document.querySelector(".modal-wrapper")
+
 
 menuBtn.addEventListener('click', () => {
-    console.log("sjkjsndjkn")
+   mobileMenu.style.left = "0";
+   
+   
 });
+
+closeSideBar.addEventListener('click', () => {
+    mobileMenu.style.left = "-100%";
+    console.log("tıklandı")
+})
+
+document.addEventListener('click', (e) => {
+    
+    const path = e.composedPath();
+    if( !path.includes(mobileMenu) && 
+        !path.includes(menuBtn)) {
+        mobileMenu.style.left = "-100%";
+    }
+
+    if(!e.target.closest(".modal-wrapper") && 
+        !e.target.closest(".search-button")){
+        modalSearch[0].style.visibility = "hidden";
+        modalSearch[0].style.opacity = "0";
+
+    }
+   
+})
+
+searchBtn.addEventListener('click', () => {
+    modalSearch[0].style.visibility = "visible";
+    modalSearch[0].style.opacity = "1";
+    
+})
+
+modalCloseBtn.addEventListener('click', () => {
+    modalSearch[0].style.visibility = "hidden";
+    modalSearch[0].style.opacity = "0";
+})
+
