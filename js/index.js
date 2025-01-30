93743f0ffc714247404f3e8bf2c26d4db525dde1
Cem <cmizci@gmail.com>
2025-01-30 16:40:15 +0300

    slider

--- js/index.js
+++ js/index.js
@@ -46,3 +46,62 @@ modalCloseBtn.addEventListener('click', () => {
     modalSearch[0].style.opacity = "0";
 })
 
+
+
+/*********** Slider Start ***********/
+
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+
+let slideIndex = 0;
+
+showSlides(slideIndex)
+
+setInterval(() => {
+    showSlides((slideIndex += 1))
+}, 4000);
+
+function currentSlide(n){
+    showSlides((slideIndex += n))
+}
+
+nextBtn.addEventListener('click', () => {
+    showSlides(++slideIndex)
+})
+
+prevBtn.addEventListener('click', () => {
+    showSlides(--slideIndex)
+})
+
+function showSlides(n){
+    const slides = document.getElementsByClassName("slider-item");
+    const dots =  document.getElementsByClassName("slider-dot");
+
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "")
+    }
+    
+    slides[slideIndex].style.display = "flex";
+    dots[slideIndex].className += " active"
+
+}
+
+
+
+
+
+
+
+/*********** Slider End  ************/
