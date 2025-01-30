a5950f78e43fdfa704545856b3e3778c5719c4de
Cem <cmizci@gmail.com>
2025-01-30 23:43:09 +0300

    glide.js end

--- js/glide.js
+++ js/glide.js
@@ -1,7 +1,41 @@
 const config = {
     type: 'carousel',
     startAt: 0,
-    perView: 3
+    perView: 4,
+    gap:10,
+    autoplay: 2000,
+    breakpoints : {
+        992: {
+            perView:3,
+        },
+        768: {
+            perView:2,
+        },
+        576: {
+            perView:1,
+        }
+    }
 }
 
 new Glide('.glide',config).mount()
+
+const config2 = {
+    type: 'carousel',
+    startAt: 0,
+    perView: 4,
+    gap:10,
+    autoplay: 2000,
+    breakpoints : {
+        992: {
+            perView:3,
+        },
+        768: {
+            perView:2,
+        },
+        576: {
+            perView:1,
+        }
+    }
+}
+
+new Glide('.glide2',config2).mount()
