caf00dd57b1aff599a45f7569ff5563a15bf9395
Cem <cmizci@gmail.com>
2025-02-12 22:27:32 +0300

    single-product

--- js/single-product.js
+++ js/single-product.js
@@ -3,6 +3,23 @@ const productId = localStorage.getItem("productId")
 : localStorage.setItem("productId", JSON.stringify(1));
 
 
-const products = localStorage.getItem("productId") 
+const products = localStorage.getItem("products") 
 ? JSON.parse(localStorage.getItem("products")) 
 : localStorage.setItem("products", JSON.stringify([]));
+
+const findProduct = products.find((item) => item.id === Number(productId));
+
+const productTitle = document.querySelector(".product-title");
+productTitle.innerHTML = findProduct.name;
+
+
+const newPriceDOM = document.querySelector(".new-price");
+const oldPriceDOM = document.querySelector(".old-price");
+
+newPriceDOM.innerHTML = findProduct.price.newPrice.toFixed(2);
+oldPriceDOM.innerHTML = findProduct.price.oldPrice.toFixed(2);
+
+const singleImageDOM = document.querySelector("#single-image");
+
+singleImageDOM.src = findProduct.img.singleImage;
+
